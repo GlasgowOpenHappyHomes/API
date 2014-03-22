fadab96c6c88fb034c7e92d2a0c5e572ccf2fe3d
Stuart Swindells <github@lumo.me.uk>
2014-03-22 22:07:08 +0000

    Model and controller for current reading

---
+++ api/models/CurrentReading.js
@@ -0,0 +1,18 @@
+/**
+ * Current_reading
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+	tableName: 'last_current_hour_reading',
+  adapter: 'mysql',
+  migrate: 'safe',
+	
+  attributes: {
+  	
+  }
+
+};
