0e67797f9ef70088f4dcff74f87ead600dcf39c0
Stuart Swindells <github@lumo.me.uk>
2014-03-22 20:17:38 +0000

    Refactor getStats call onto Stats model

--- api/controllers/StatsController.js
+++ api/controllers/StatsController.js
@@ -27,53 +27,11 @@ module.exports = {
   _config: {},
 	
   getStats: function(req, res, next) {
+		var stats = Stats.fetch(req.query.location_id, req.query.dow, req.query.hour, gotStats)
 		
-		if (req.query.hour) {
-			req.query.hour = parseInt(req.query.hour)
+		function gotStats(result)
+		{
+			res.json(result);
 		}
-		
-		if (req.query.dow) {
-			req.query.dow = parseInt(req.query.dow)
-		}
-		
-		if (req.query.location_id) {
-			req.query.location_id = parseInt(req.query.location_id)
-		}
-		
-		
-		var StatsHelper = require('fast-stats').Stats;
-		
-		var data = []
-		//req.query = {location_id: req.query.location_id, dow: req.query.dow, hour: req.query.hour}
-		
-
-		
-    Stats.find(req.query).exec(function(err, result){
-			result.forEach(function(entry) {
-				
-				data.push(entry.reading)
-			});
-
-		
-			if (data.length === 0) {
-				res.json();
-			}
-			
-			statsObj = new StatsHelper().push(data)
-		
-			res.json(
-				{
-					size: data.length,
-					min: statsObj.range()[0],
-					lower_whisker: statsObj.percentile(9),
-					lower_quartile: statsObj.percentile(25),
-					median: statsObj.median(),
-					upper_quartile: statsObj.percentile(75),
-					upper_whisker: statsObj.percentile(91),
-					max: statsObj.range()[1],
-					rawData: data
-				}
-			);
-    });
-  }
+	}
 };
--- api/models/Stats.js
+++ api/models/Stats.js
@@ -16,6 +16,40 @@ module.exports = {
 		
 		// get attributes automatically
     
-  }
+  },
+	
+	fetch: function(location_id, day_of_week, hour_of_day, successfunc){
+		var StatsHelper = require('fast-stats').Stats;
+		
+		var data = []		
+		
+    this.find(
+			{location_id: location_id, dow: day_of_week, hour: hour_of_day}
+		).exec(function(err, result){
+			result.forEach(function(entry) {
+				data.push(entry.reading)
+			});
 
+		
+			if (data.length === 0) {
+				successFunc({})
+			}
+			
+			statsObj = new StatsHelper().push(data)
+		
+			var resultObj = {
+					size: data.length,
+					min: statsObj.range()[0],
+					lower_whisker: statsObj.percentile(9),
+					lower_quartile: statsObj.percentile(25),
+					median: statsObj.median(),
+					upper_quartile: statsObj.percentile(75),
+					upper_whisker: statsObj.percentile(91),
+					max: statsObj.range()[1],
+					rawData: data
+				}
+				
+				successfunc(resultObj);
+    });
+	}
 };
